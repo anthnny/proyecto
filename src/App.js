f70fae1f9251cdf24466698b2f14918fb9c1afa4
anthnny <zanthonygg@gmail.com>
2023-07-25 14:09:24 -0500

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-import { NavBar } from './components/NavBar';
-import { Banner } from './components/Banner';
-import { Info } from './components/Info';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 function App() {
   return (
-    <div className="App">
-      <NavBar />
-      <Banner />
-      <Info />
+    <div>
+      hola mundo
     </div>
   );
 }
